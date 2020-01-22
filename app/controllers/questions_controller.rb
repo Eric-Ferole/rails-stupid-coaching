33ea3ffffdd1cde866453c04d2c4edf16c140d52
Éric Férole <eferole@pixelcircus.ca>
2020-01-21 20:59:12 -0500

    Routes en routes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,17 @@
 class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @message = params[:question]
+
+    if @message == 'I am going to work'
+      @answer = 'Great!'
+    elsif @message.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = " "
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get 'ask', to: 'questions#index'
-    get 'answer', to: 'questions#index'
+    get 'ask', to: 'questions#ask'
+    get 'answer', to: 'questions#answer'
+
+    # post 'aks', to: 'questions#create'
+    # post 'answer', to: 'questions#create'
 
 end
+
